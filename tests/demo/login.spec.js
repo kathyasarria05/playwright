ea34f009fe8e05d7d04f08eb57afc7a785e4bd84
Kathya Sarria <kathyasarria@MacBook-Pro-de-Pruebas.local>
2025-04-11 15:30:05 -0600

    first commit

---
+++ tests/demo/login.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/v1/');
+  console.log('test');
+  await page.locator('#user-name').fill('standard_user');
+  await page.locator('#password').fill('secret_sauce');
+  await page.locator('#login-button').click();
+  await expect(page).toHaveURL('https://www.saucedemo.com/v1/inventory.html');
+
+
+});
+
+
+
