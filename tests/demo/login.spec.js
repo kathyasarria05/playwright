12aa62f093f561c5ee1dc6dc0395f84c99b7a1b6
Kathya Sarria <kathyasarria@MacBook-Pro-de-Pruebas.local>
2025-04-11 15:44:08 -0600

    Second Commit

--- tests/demo/login.spec.js
+++ tests/demo/login.spec.js
@@ -10,6 +10,16 @@ test('test', async ({ page }) => {
 
 
 });
+//Second Commit
+// test('test', async ({ page }) => {
+//   await page.goto('https://www.saucedemo.com/v1/');
+//   console.log('test');
+//   await page.locator('#user-name').fill('standard_user');
+//   await page.locator('#password').fill('secret_sauce');
+//   await page.locator('#login-button').click();
+//   await expect(page).toHaveURL('https://www.saucedemo.com/v1/inventory.html');
 
+
+// });
 
 
