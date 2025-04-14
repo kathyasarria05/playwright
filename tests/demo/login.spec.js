7de66394d0512218ae92c37d3b6d431da1492cca
Kathya Sarria <kathyasarria@MacBook-Pro-de-Pruebas.local>
2025-04-14 17:11:10 -0600

    login tests

--- tests/demo/login.spec.js
+++ tests/demo/login.spec.js
@@ -1,25 +1,22 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../pages/login';
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/v1/');
-  console.log('test');
-  await page.locator('#user-name').fill('standard_user');
-  await page.locator('#password').fill('secret_sauce');
-  await page.locator('#login-button').click();
+test('Login with valid credentials', async ({ page }) => { 
+
+  const loginPage = new LoginPage(page);
+  await loginPage.goToLoginPage();
+  await loginPage.login('standard_user', 'secret_sauce');
   await expect(page).toHaveURL('https://www.saucedemo.com/v1/inventory.html');
 
-
 });
-//Second Commit
-// test('test', async ({ page }) => {
-//   await page.goto('https://www.saucedemo.com/v1/');
-//   console.log('test');
-//   await page.locator('#user-name').fill('standard_user');
-//   await page.locator('#password').fill('secret_sauce');
-//   await page.locator('#login-button').click();
-//   await expect(page).toHaveURL('https://www.saucedemo.com/v1/inventory.html');
 
+// //test with no page object
+// test('Login with invalid credentials', async ({ page }) => {
+//   await page.goto('https://www.saucedemo.com/v1/');
+//   await page.locator('#user-name').fill('standarduser');
+//   await page.locator('#password').fill('secretsauce');
+//   await page.locator('#login-button').click();
+//   await expect(page.locator('h3[data-test="error"]')).toContainText('Epic sadface: Username and password do not match any user in this service');
 
 // });
-
 
